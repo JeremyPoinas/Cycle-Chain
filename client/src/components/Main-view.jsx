99f0cb4a15d15c33cbb4eadc287489167dd8de27
Thomas DOUCHE <thomas.douche@aol.com>
2022-12-05 17:00:16 +0100

    Creation d une main view

--- client/src/components/Main-view.jsx
+++ client/src/components/Main-view.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import PartsTable from "./Parts-list";
 import EquipmentsGrid from "./Equipments-grid";
 
 export default function MainView() {
     return (
-        <div>
-            <EquipmentsGrid />
-            <PartsTable />
-        </div>
-        
+        <Box sx={{ width: '100%', maxWidth: 2000, p:5 }}>
+
+            <Box sx={{ mb:5 }}>
+                <Typography variant="h4" gutterBottom>Équipements</Typography>
+                <EquipmentsGrid />
+            </Box>
+
+            <Box sx={{ mb:5 }}>
+                <Typography variant="h4" gutterBottom>Pièces</Typography>
+                <PartsTable />
+            </Box>
+            
+        </Box>
+            
     )
 }
 
