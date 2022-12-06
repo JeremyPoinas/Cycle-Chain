e303b1809061dfde3e7053e74ff1452ca149332b
JeremyPoinas <poinas.jeremy@gmail.com>
2022-12-06 16:00:52 +0100

    jp/implemented routes with React router DOM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,40 @@
+import { useEffect } from 'react';
+import {
+  Routes, Route, useLocation,
+} from 'react-router-dom';
 import { EthProvider } from "./contexts/EthContext";
-//import Header from "./components/Header";
+import Header from "./components/Header";
 //import Footer from "./components/Footer";
 import EquipmentsGrid from "./components/Equipments-grid";
-import OperationsList from "./components/Operations-list";
+import EquipmentDetail from "./components/Equipment-detail";
+import Page404 from "./components/Page404";
+import About from "./components/About";
 import "./App.css";
 
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, [location]);
+
   return (
     <EthProvider>
       <div id="App" >
         <div className="container">
-            <EquipmentsGrid />
-            <OperationsList />
+            <Header />
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/my-portfolio" element={<EquipmentsGrid />} />
+              <Route path="/update-equipment" element={<EquipmentDetail />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
         </div>
       </div>
     </EthProvider>
---
+++ client/src/components/About.jsx
@@ -0,0 +1,20 @@
+const About = () => (
+  <div className="home__container">
+    <h3 className="home__title">Welcome on CycleChain</h3>
+    <div className="home__description">
+      <p>
+        The Construction Equipment Industry:<br></br>
+        - A $136 Billion market in sales,<br></br>
+        - 270,000 equipment/vehicles produced per year; 10 to 100 parts per year per equipment concerned by Cycle Chain<br></br>
+        - Spare parts represent $27 billion of which $4 billion is counterfeit, which escapes the manufacturers<br></br><br></br>
+
+        Cycle Chain's objectives:<br></br>
+        - Recover a part of this segment by certifying and tracking parts via the sale of NFTs<br></br>
+        - Solution accessible to all: equipment manufacturers, parts manufacturers/resellers and end customers<br></br>
+      </p>
+      
+    </div>
+  </div>
+);
+
+export default About;
