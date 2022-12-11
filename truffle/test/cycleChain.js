dc1fb8ea39c4b6d7ac055752d7cbc36c8acc939d
JeremyPoinas <poinas.jeremy@gmail.com>
2022-12-11 19:30:30 +0100

    test

--- truffle/test/cycleChain.js
+++ truffle/test/cycleChain.js
@@ -1,7 +1,24 @@
 const CycleChain = artifacts.require("CycleChain");
 
-contract('CycleChain', () => {
-  it('should read newly written values', async() => {
-    const CycleChainInstance = await CycleChain.deployed();
+
+const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+
+contract('CycleChain', accounts => {
+  const [owner, second, third] = accounts;
+
+  let ContractInstance;
+
+  // ::::::::::::: VOTER REGISTRATION ::::::::::::: //
+
+
+  describe("Voter registration", function () {
+
+    let voter;
+
+    beforeEach(async function () {
+      // Create a new contract instance
+      ContractInstance = await CycleChain.new({from:owner});
+    });
   });
 });
